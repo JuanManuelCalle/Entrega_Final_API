023246e411585be9b8596d93b304c19b3d2503eb
JuanManuelCalle <juancalle478@gmail.com>
2022-10-27 23:11:13 -0500

    Se elimina carpeta Erronea

--- ConsignaFinal/Entrega_Final_API-Master/ConsignaFinal/ConsignaFinal/Models/Login.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConsignaFinal.Models
-{
-    public class Login
-    {
-        public int id { get; set; }
-        public string Usuario { get; set; }
-        public string Contrasena { get; set; }
-    }
-}
